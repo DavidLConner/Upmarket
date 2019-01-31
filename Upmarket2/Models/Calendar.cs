5c11bf50cafa7144738f82fb874655d9008ffac8
David Conner <angstart8@gmail.com>
2019-01-31 14:37:11 -0600

    Correct welcome

---
+++ Upmarket2/Models/Calendar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Upmarket2.Models
+{
+    public class Calendar
+    {
+        public bool IsWednesday { get; set; }
+
+        public bool IsSaturday { get; set; }
+    }
+}
