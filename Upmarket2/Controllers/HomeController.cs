e2ec1c8f1fdac298ee859b7addedb0c22da5fa87
David Conner <angstart8@gmail.com>
2019-02-16 08:58:06 -0600

    database, screenshot, login/register ok

--- Upmarket2/Controllers/HomeController.cs
+++ Upmarket2/Controllers/HomeController.cs
@@ -15,7 +15,9 @@ namespace Upmarket2.Controllers
             return View();
         }
 
-    
+
+
+  
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
