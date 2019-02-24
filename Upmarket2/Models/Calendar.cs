5deeeea9e3d60852fa7a675437adf13cdaefc0ff
David Conner <angstart8@gmail.com>
2019-02-23 21:50:01 -0600

    with CSS

--- Upmarket2/Models/Calendar.cs
+++ Upmarket2/Models/Calendar.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace Upmarket2.Models
 {
@@ -10,5 +12,6 @@ namespace Upmarket2.Models
         public bool Wednesday { get; set; }
 
         public bool Saturday { get; set; }
+
     }
 }
